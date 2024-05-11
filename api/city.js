ddbfb98e5b87fa5f968c85596015a6014175644c
นายคเชนทร์ จันทเกษ <164714767+Kj-work68@users.noreply.github.com>
2024-05-11 15:37:03 +0700

    update
    th-city.json  & lon,lat

--- api/city.js
+++ api/city.js
@@ -2,7 +2,6 @@ const axios = require("axios")
 const settings = require("../setting")
 const city  = (cityName = "Bangkok", countryCode = "th") =>{
     return axios
-    .get(`${settings.apiUrl}?q=${cityName},${countryCode}
-    &appid=${settings.apiKey}&units=metric`)
+    .get(`${settings.apiUrl}?q=${cityName},${countryCode}&appid=${settings.apiKey}&units=metric`)
 }
 module.exports = city
--- api/coordinate.js
+++ api/coordinate.js
@@ -1,8 +1,7 @@
 const axios = require('axios')
 const settings = require('../setting')
-const location = (lon = 102.3829839 ,lat = 15.6338096 ) =>{
+const location = (lon=102.3829839 ,lat=15.6338096 ) =>{
 return axios 
-    .get(`${settings.apiUrl}?lon=${lon}&lat=${lat}
-    &appid=${settings.apiKey}&units=metric`)
+    .get(`${settings.apiUrl}?lon=${lon}&lat=${lat}&appid=${settings.apiKey}&units=metric`)
 }
 module.exports = location;
